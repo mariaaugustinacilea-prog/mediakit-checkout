b47d4c7f76cb200de3b25b261c706ecac0edd4e5
mariaaugustinacilea-prog <mariaaugustina.cilea@gmail.com>
2026-04-02 19:38:22 +0300

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Allow CORS from your Carrd site
-  res.setHeader('Access-Control-Allow-Origin', 'https://mediaproducts.net');
+  res.setHeader('Access-Control-Allow-Origin',  '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
